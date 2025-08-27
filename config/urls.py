7b1939293724eac85bdbd1cac1382b935856428f
Mbugualawrencee <143984474+kb-diplo@users.noreply.github.com>
2025-08-27 09:23:05 +0300

    Configure whitenoise for static and media files in production

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,8 +15,12 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/', permanent=True)),
 ]
 
-# Serve static and media files in development
+# Serve static and media files
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Debug only URLs
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")))
